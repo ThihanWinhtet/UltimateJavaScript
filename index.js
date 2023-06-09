bde2e11ed947dbe28b49e7c21a1904dc001df902
Thihan Winhtt <thihanwinhtet1999@gmail.com>
2023-06-09 16:37:49 +0630

    Exercise no3 FizzBuzz.

--- index.js
+++ index.js
@@ -14,8 +14,23 @@
 // console.log(maxNum, "testing the branch");
 
 // 2 Landscape or Portrait
-console.log(isLandscape(12,40));
-function isLandscape(width , height){
-    return(width > height);
+// console.log(isLandscape(12,40));
+// function isLandscape(width , height){
+//     return(width > height);
+// }
+
+// 3 FizzBuzz
+console.log(FizzBuzz(15));
+function FizzBuzz(input){
+    if(typeof(input) !== 'number')
+        return 'Not a number';
+    if(input % 3 == 0 && input % 5 == 0)
+        return 'FizzBuzz'
+    else if(input % 3 == 0)
+        return 'Fizz';
+    else if(input % 5 == 0)
+        return 'Buzz';
+    
+    return input;    
 }
 

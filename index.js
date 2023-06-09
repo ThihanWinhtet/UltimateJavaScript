66b82f14bc0ffee6e11f1e6e546fcc5e7794f667
Thihan Winhtt <thihanwinhtet1999@gmail.com>
2023-06-09 16:22:03 +0630

    Exercise no2 Landscape or not.

--- index.js
+++ index.js
@@ -2,14 +2,20 @@
 // Control Flow Exercises
 
 // 1 Max of Two Numbers
-var maxNum;
-function maxOf(int1 , int2){
-    if(int1 > int2){
-        maxNum = int1;
-    }else{
-        maxNum = int2;
-    }
+// var maxNum;
+// function maxOf(int1 , int2){
+//     if(int1 > int2){
+//         maxNum = int1;
+//     }else{
+//         maxNum = int2;
+//     }
+// }
+// maxOf(5,4);
+// console.log(maxNum, "testing the branch");
+
+// 2 Landscape or Portrait
+console.log(isLandscape(12,40));
+function isLandscape(width , height){
+    return(width > height);
 }
-maxOf(5,4);
-console.log(maxNum, "testing the branch");
 

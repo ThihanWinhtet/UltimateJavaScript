6eb9da3051685858eb7a01b0491c889056be5466
Thihan Winhtt <thihanwinhtet1999@gmail.com>
2023-06-09 16:02:34 +0630

    Exercise no1 Max Of Two Nums.

---
+++ index.js
@@ -0,0 +1,15 @@
+
+// Control Flow Exercises
+
+// 1 Max of Two Numbers
+var maxNum;
+function maxOf(int1 , int2){
+    if(int1 > int2){
+        maxNum = int1;
+    }else{
+        maxNum = int2;
+    }
+}
+maxOf(5,4);
+console.log(maxNum);
+

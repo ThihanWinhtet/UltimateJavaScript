094fe83fecc4d3d9a342ae13788a9232d5fc772a
Thihan Winhtt <thihanwinhtet1999@gmail.com>
2023-06-12 13:15:55 +0630

    Constructor Function.

--- objects.js
+++ objects.js
@@ -12,3 +12,18 @@ function createCircle(radius){
 
 let circle1 = createCircle(1);
 console.log(circle1);
+
+// Constructor Function 
+
+function Circle(radius){
+    this.radius = radius;
+    this.draw = function(){
+        console.log("draw me");
+    }
+}
+
+const circle = new Circle(4);
+// -*- when we call the "new" operator three things happen
+// -*- create the empty js object
+// -*- it will set "this" to the new empty object
+// -*- will return new object from the constructor function

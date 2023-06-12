2fa1c4420bb883cc06006ef73952ab068fd0534d
Thihan Winhtt <thihanwinhtet1999@gmail.com>
2023-06-12 09:10:23 +0630

    Exercise no4 Demerit Points

--- index.js
+++ index.js
@@ -20,17 +20,34 @@
 // }
 
 // 3 FizzBuzz
-console.log(FizzBuzz(15));
-function FizzBuzz(input){
-    if(typeof(input) !== 'number')
-        return 'Not a number';
-    if(input % 3 == 0 && input % 5 == 0)
-        return 'FizzBuzz'
-    else if(input % 3 == 0)
-        return 'Fizz';
-    else if(input % 5 == 0)
-        return 'Buzz';
+// console.log(FizzBuzz(15));
+// function FizzBuzz(input){
+//     if(typeof(input) !== 'number')
+//         return 'Not a number';
+//     if(input % 3 == 0 && input % 5 == 0)
+//         return 'FizzBuzz'
+//     else if(input % 3 == 0)
+//         return 'Fizz';
+//     else if(input % 5 == 0)
+//         return 'Buzz';
     
-    return input;    
+//     return input;    
+// }
+
+// 4 Demerit Points 
+console.log(checkSpeed(100));
+function checkSpeed(speed){
+    const speedLimitActive = 75;
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+    const limitedPoint = 12;
+
+    if(speed >= speedLimitActive){
+        if(Math.floor((speed - speedLimit) / kmPerPoint) >= limitedPoint){
+            return 'License Suspended';
+        }
+        return 'Point ' + Math.floor((speed - speedLimit) / kmPerPoint);
+    }
+    return 'OK'
 }
 

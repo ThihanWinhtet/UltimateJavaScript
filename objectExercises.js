3d24e3db715d30b6b33b7df97874fdc58455d7c8
Thihan Winhtt <thihanwinhtet1999@gmail.com>
2023-06-12 15:56:56 +0630

    Objects Exercise no2.

--- objectExercises.js
+++ objectExercises.js
@@ -12,3 +12,19 @@
 //         console.log(item, address[item]);
 //     }
 // }
+
+// no2
+function createAddress(){
+    return {
+        street : 'BelAir',
+        city : 'Zonamia',
+        zipCode : '198zC'
+    }
+}
+
+showAddress(createAddress);
+function showAddress(obj){
+    for(let item in address){
+        console.log(item, address[item]);
+    }
+}

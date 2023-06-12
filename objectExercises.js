5ef44bb94552fc6b999a5c084eddeddd84f378d4
Thihan Winhtt <thihanwinhtet1999@gmail.com>
2023-06-12 15:46:18 +0630

    obj exercise no2

--- objectExercises.js
+++ objectExercises.js
@@ -1,16 +1,30 @@
 
 // no1
-const address = {
-    street : 'BelAir',
-    city : 'Zonamia',
-    zipCode : '198zC'
+// const address = {
+//     street : 'BelAir',
+//     city : 'Zonamia',
+//     zipCode : '198zC'
+// }
+
+// showAddress(address);
+// function showAddress(obj){
+//     for(let item in address){
+//         console.log(item, address[item]);
+//     }
+// }
+
+// no2
+function createAddress(){
+    return {
+        street : 'BelAir',
+        city : 'Zonamia',
+        zipCode : '198zC'
+    }
 }
 
-showAddress(address);
+showAddress(createAddress);
 function showAddress(obj){
     for(let item in address){
         console.log(item, address[item]);
     }
 }
-
-// no2

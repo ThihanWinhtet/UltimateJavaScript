6e6abc2cdbc0dd291aa8f142c6ef1d71fa8764fb
Thihan Winhtt <thihanwinhtet1999@gmail.com>
2023-06-12 16:24:30 +0630

    Object Exercise no5

--- objectExercises.js
+++ objectExercises.js
@@ -51,15 +51,29 @@
 // }
 
 // no4
-let post = {
-    title : 'JS Part1',
-    body : ' return address1.street',
-    author : 'c',
-    views : 0,
-    comments : [
-        {author : 'c', body : 'b'},
-        {author : 'c', body : 'b'}
-    ],
-    isLive : true
-}
+// let post = {
+//     title : 'JS Part1',
+//     body : ' return address1.street',
+//     author : 'c',
+//     views : 0,
+//     comments : [
+//         {author : 'c', body : 'b'},
+//         {author : 'c', body : 'b'}
+//     ],
+//     isLive : true
+// }
+// console.log(post);
+
+// no5
+let post = new Post('a', 'b', 'c');
 console.log(post);
+function Post(title, body, author){
+    this.title = title;
+    this.body = body;
+    this.author = author;
+    this.views = 0;
+    this.comments = [
+        {author : 'c', body : 'b'},
+    ];
+    this.isLive = true;
+}

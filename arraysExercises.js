8cbf482e75e1a7f43861a0b511dc43eba6235a92
Thihan Winhtt <thihanwinhtet1999@gmail.com>
2023-06-13 14:22:40 +0630

    Array Exercise no2 Includes.

--- arraysExercises.js
+++ arraysExercises.js
@@ -1,11 +1,26 @@
 // no1 Array From Range
+// const number = arrayFromRange(1,-4);
+// 
+// console.log(number);
+// function arrayFromRange(min, max){
+//     let array = [];
+//     for(let i = min; i <= max ; i++){
+//         array.push(i);
+//     }
+//     return array;
+// }
 
-const number = arrayFromRange(1,-4);
-console.log(number);
-function arrayFromRange(min, max){
-    let array = [];
-    for(let i = min; i <= max ; i++){
-        array.push(i);
+
+// no2 Includes
+const number = [1,2,33,4];
+
+console.log(includes(number,33));
+
+function includes(array , searchElement){
+    for(let item of array){
+        if(item === searchElement){
+            return true;
+        }
     }
-    return array;
+    return false;
 }

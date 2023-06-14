740f06baf205b6f728babbda1ac98882cc819477
Thihan Winhtt <thihanwinhtet1999@gmail.com>
2023-06-14 10:11:47 +0630

    Function Exercise no2 Area of Circle.

--- functionExercises.js
+++ functionExercises.js
@@ -8,3 +8,12 @@ function sum(...args){
     args.forEach(item => sum += item);
     return sum;
 }
+
+// no2 Area of Circle
+const circle = {
+    radius : 2,
+    get area(){
+        return Math.PI * this.radius * this.radius;
+    }
+}
+console.log(circle.area);
